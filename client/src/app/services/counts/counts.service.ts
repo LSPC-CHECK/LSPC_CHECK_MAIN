13f37088d110de55cc225adb1a8aff6713c43b9b
Andres Escobar Vélez <aescobar@hematologico.com>
2023-09-03 18:58:22 -0500

    Fix bad url gateway

--- client/src/app/services/counts/counts.service.ts
+++ client/src/app/services/counts/counts.service.ts
@@ -3,41 +3,41 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountsService {
-
-  EntSal = 'http://localhost:4000/entSal/count';
-  Computer = 'http://localhost:4000/computer/count';
-  Pqrs = 'http://localhost:4000/pqrs/count';
-  User = 'http://localhost:4000/user/count';
-  Profile = 'http://localhost:4000/profile/count';
+  baseUrl = 'http://localhost:4000/api';
+  EntSal = `${this.baseUrl}/entSal/count`;
+  Computer = `${this.baseUrl}/computer/count`;
+  Pqrs = `${this.baseUrl}/pqrs/count`;
+  User = `${this.baseUrl}/user/count`;
+  Profile = `${this.baseUrl}/profile/count`;
 
   token = localStorage.getItem('token');
   //headers donde enviamos el token para ser autorizados
   httpOptions = {
-    headers : new HttpHeaders({
-      'Content-type':'application/json',
-      'Accept':'application/json',
-      'Authorization': `Bearer ${this.token}`,
-    })
-  }
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    }),
+  };
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getCountEntSal():Observable<any>{
-    return this.http.get(this.EntSal,this.httpOptions);
+  getCountEntSal(): Observable<any> {
+    return this.http.get(this.EntSal, this.httpOptions);
   }
-  getCountComputers():Observable<any>{
-    return this.http.get(this.Computer,this.httpOptions);
+  getCountComputers(): Observable<any> {
+    return this.http.get(this.Computer, this.httpOptions);
   }
-  getCountPqrs():Observable<any>{
-    return this.http.get(this.Pqrs,this.httpOptions);
+  getCountPqrs(): Observable<any> {
+    return this.http.get(this.Pqrs, this.httpOptions);
   }
-  getCountUsers():Observable<any>{
-    return this.http.get(this.User,this.httpOptions);
+  getCountUsers(): Observable<any> {
+    return this.http.get(this.User, this.httpOptions);
   }
-  getCountProfile():Observable<any>{
-    return this.http.get(this.Profile,this.httpOptions);
+  getCountProfile(): Observable<any> {
+    return this.http.get(this.Profile, this.httpOptions);
   }
 }
