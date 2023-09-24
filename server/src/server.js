258a9c2c5960c0b7035c5260ce4c169d469617d9
Andres Escobar Vélez <aescobar@hematologico.com>
2023-09-24 02:01:44 -0500

    run prettier formatter and fix the limit of the request to 50mb

--- server/src/server.js
+++ server/src/server.js
@@ -9,31 +9,31 @@ const passwordForgetRoutes = require("./routes/forgetpasw.routes");
 
 global.__basedir = __dirname;
 
-db.sequelizeCfg.sync()
-    .then(() =>{
-        console.log("Synced DB");
-    })
-    .catch((err) =>{
-        console.log("failed to sync db:"+ err.message);
-    });
+db.sequelizeCfg
+	.sync()
+	.then(() => {
+		console.log("Synced DB");
+	})
+	.catch((err) => {
+		console.log("failed to sync db:" + err.message);
+	});
 
-
-var corsOptions ={
-    origin:true
-}
+var corsOptions = {
+	origin: true,
+};
 
 app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true, parameterLimit: 50000 }));
 initRoutes(app);
 
-app.get("/",(req,res)=>{
-    res.json({message: "Bienvenido!"});
+app.get("/", (req, res) => {
+	res.json({ message: "Bienvenido!" });
 });
 
 const PORT = process.env.PORT || 4000;
@@ -48,6 +48,6 @@ require("./routes/image.routes")(app);
 app.use(passwordResetRoutes); // agregar la ruta de reinicio de contraseña
 app.use(passwordForgetRoutes); // agregar la ruta de olvido de contraseña
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}.`);
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}.`);
 });
