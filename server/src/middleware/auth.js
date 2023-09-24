207da748141694689edb3b84ed849f2502d1b0f6
Andres Escobar Vélez <aescobar@hematologico.com>
2023-09-24 02:02:42 -0500

    Run prettier

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,14 +1,14 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // Funcion para verifcar el token y autirazr el acceso a la base de datos
-exports.verifyToken = function(req,res,next) {
-  if (!req.headers.authorization) return res.status(401).json('No autorizado');
-  const token = req.headers.authorization.substr(7);
-  if (token!=='') {
-    const content = jwt.verify(token, process.env.AUTH_TOKEN_KEY);
-    req.data = content;
-    next()
-  } else{
-    res.status(401).json('Token vacio')
-  }
-}
+exports.verifyToken = function (req, res, next) {
+	if (!req.headers.authorization) return res.status(401).json("No autorizado");
+	const token = req.headers.authorization.substr(7);
+	if (token !== "") {
+		const content = jwt.verify(token, process.env.AUTH_TOKEN_KEY);
+		req.data = content;
+		next();
+	} else {
+		res.status(401).json("Token vacio");
+	}
+};
